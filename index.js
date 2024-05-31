13fa85267f78ac0924e4ff2b08d3844a19d34d0f
lynne <125513875+therapist18@users.noreply.github.com>
2024-05-31 17:44:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper(".swiper1", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2.75,
+    },
+  },
+});
+
+var swiper = new Swiper(".swiper2", {
+  grabCursor: true,
+  // centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
