a57391e57b72b29c6cb08b5e6593d0133d82818c
lynne <125513875+therapist18@users.noreply.github.com>
2024-06-05 15:29:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -49,3 +49,63 @@ var swiper = new Swiper(".swiper2", {
     },
   },
 });
+document.addEventListener("DOMContentLoaded", function() {
+  var sticky_top = document.getElementById("sticky_top");
+
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 50) {
+      sticky_top.classList.add("scrolled");
+    } else {
+      sticky_top.classList.remove("scrolled");
+    }
+  });
+});
+
+var swiper = new Swiper(".swiper3", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".swiper4", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 5,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
